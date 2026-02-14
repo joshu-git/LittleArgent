b817763ca4a342b22282d08cae9d30f7f2471517
joshu-git <argentjackjoshua@outlook.com>
2026-02-14 16:22:37 +0000

    Remove yearlygo project page
    Project has been discontinued

---
+++ frontend/src/app/projects/littleflows/page.tsx
@@ -0,0 +1,13 @@
+export default function LittleFlowsPage() {
+	return (
+		<div className="container py-16 space-y-20">
+			{/* HEADER */}
+			<section className="max-w-3xl space-y-6">
+				<h1 className="text-4xl md:text-5xl font-extrabold tracking-tight">
+					LittleFlows
+				</h1>
+				<p className="text-lg">Coming soon...</p>
+			</section>
+		</div>
+	);
+}
